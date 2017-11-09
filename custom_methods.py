decf030293fb2974028cb4ce616362ea28a77185
rahulk <rahulkmr.kmr75@gmail.com>
2017-11-09 19:23:56 +0530

    Spectogram and segmentation using VAD done

---
+++ custom_methods.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2
+import math
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+def gen_spectogram_image(spec):
+	rows = len(spec)
+	cols = len(spec[0])
+	img = np.zeros((rows,cols),dtype= np.uint8)
+
+	'''
+	assuming the values in the trasform varies from 10^-14 to 10^2
+	the corresponding non-linear mapping gave the equation
+	y = 16 log10(x) + 224
+	'''
+	for i in range(0,rows):
+		for j in range(0,cols):
+			try:
+				img[i][j] = int(math.log10(spec[i][j]) * 16 + 224)
+			except ValueError:
+				img[i][j] = 0
+
+	return img
+
+def gen_energy_histogram(ener):
+	'''normally log of energy for a frame is not more than 3 and for many
+	silence parts it is as low as -8 so bins from bins have been taken 
+	[-12 , -12 + 0.17d], here d = {0,1,2,...,99}'''
+
+	y = np.linspace(-12 , 5 , num=100)
+	plt.hist(ener , y , alpha = 0.5)
+	plt.show()
+
+def estimate_threshold_VAD(ener):
+	'''it was observed that the log energies of the frame approximates somewhat
+	a mixture of two gaussians or two clusters, and the same was used to estimate 
+	the threshold
+	'''
+	N = 2
+
+	'''CV = 'full'
+	gmm = mixture.GaussianMixture(n_components = N , covariance_type = CV)
+	gmm.fit(ener)
+	'''
+
+	kmeans = KMeans(n_clusters = N , random_state = 0).fit(ener)
+	return kmeans
---
+++ main.py
@@ -0,0 +1,79 @@
+import essentia
+from essentia.standard import *
+import cv2
+import sys
+import os
+import numpy as np
+from pylab import plot, show, figure, imshow
+import matplotlib.pyplot as plt
+from custom_methods import *
+import math
+
+plt.rcParams['figure.figsize'] = (15, 6) # set plot sizes to something larger than default
+
+fn = sys.argv[1]		#audio file name
+sr = int(sys.argv[2])		#sampling rate of the audio
+
+loader=essentia.standard.MonoLoader(filename =fn ,sampleRate = sr)
+audio = loader()		#audio sequence
+n_samples = len(audio)		#number of samples in the audio
+
+w =  Windowing(type = 'hann')
+wl = sr/1000*25			#window length considering a window of 25ms
+overlap = sr/1000*10		#overlap between two consecutive windows 10ms
+
+
+spectrum = Spectrum(size = wl)
+energy = Energy()
+ener=[]
+
+spectogram = []
+fstart = 0
+fstop = fstart+wl
+while fstop < n_samples:
+	frame = audio[fstart:fstop]
+	e = math.log10(energy(frame))
+	ener.append(e)		#log of total energy is being taken
+
+	fstart = fstart + overlap
+	fstop = fstop + overlap
+
+
+#gen_energy_histogram(ener)		#draw and plot the histogram of energies
+
+temp = np.array(ener)
+temp = temp.reshape(-1,1)		#clustering demands the matrix in certain fashion
+threshold = estimate_threshold_VAD(temp)	#threshold contains various GMM parametes
+print threshold.labels_
+						#use predict_proba(x) to evaluate posteriori
+
+spectogram = []			#this contains spectogram of all the frames
+spectogram1 = []		#this contaisn spectogram of only speech portions
+fstart = 0
+fstop = fstart+wl
+while fstop < n_samples:
+	frame = audio[fstart:fstop]
+	ener = [math.log10(energy(frame))]		#log of total energy is being taken
+	spec = spectrum(w(frame))
+
+	a = threshold.predict([ener])
+	print a
+	spectogram.append(spec)
+	#write your code here to separate the two spectograms
+	#the one written below is hardcoded one
+	if(a[0] == 0):
+		spectogram1.append(spec)
+
+	fstart = fstart + overlap
+	fstop = fstop + overlap
+
+
+spectogram = np.array(spectogram)
+img = gen_spectogram_image(spectogram)
+img1 = gen_spectogram_image(spectogram1)
+cv2.imshow("all",img)
+cv2.imshow("speech",img1)
+cv2.waitKey(0)
+
+
+cv2.destroyAllWindows()
